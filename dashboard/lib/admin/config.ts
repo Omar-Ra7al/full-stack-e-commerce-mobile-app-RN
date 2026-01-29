d8ceebb850937d8f77d1516baa43637530eb17ec
Omar-Ra7al <syntaxor7@gmail.com>
2026-01-30 00:34:52 +0200

    chore: added env and comment the storage until we handel the billing acc

--- dashboard/lib/admin/config.ts
+++ dashboard/lib/admin/config.ts
@@ -7,10 +7,10 @@ if (!admin.apps.length) {
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
     }),
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    // storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   });
 }
 
 export const auth = admin.auth();
 export const db = admin.firestore();
-export const bucket = admin.storage().bucket();
+// export const bucket = admin.storage().bucket();
